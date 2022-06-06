8fd65143a6695b9a9b066e51f92bb078c2c0ce73
Shawn Wilson <swilson@swilson-macbookpro-2019.lan>
2022-06-05 23:51:24 -0400

    timer functionality added

--- klymur/src/app/app.component.ts
+++ klymur/src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'klymur';
+
+  _start: boolean=false;
+  constructor(){
+  }
+
+  ngOnInit(){
+  }
+
+  start(){
+    this._start=true;
+  }
+  clear(){
+    this._start=false;
+  }
 }
