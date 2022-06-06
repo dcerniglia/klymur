b560914a62e9f24914563a682c5bdac9f884a2a1
Jeremiah Williams <jeremiahwilliams@Jeremiahs-MacBook-Air.local>
2022-06-05 22:49:14 -0400

    added actual functionality to the route counter

--- klymur/src/app/components/route-counter/route-counter.component.ts
+++ klymur/src/app/components/route-counter/route-counter.component.ts
@@ -7,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RouteCounterComponent implements OnInit {
 
+  countNumber = 0;
+
+  resetBtn()
+  {
+    this.countNumber = 0;
+  }
+
+  increaseBtn()
+  {
+    this.countNumber = this.countNumber + 1;
+  }
+
+  decreaseBtn(){
+    this.countNumber = this.countNumber - 1;
+    if(this.countNumber<0) {
+      this.countNumber = 0;
+    }
+  }
+
   constructor() { }
 
   ngOnInit(): void {
