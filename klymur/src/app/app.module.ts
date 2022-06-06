d887345f17d8e41ac18a3a0061eec64de5844436
Jeremiah Williams <jeremiahwilliams@Jeremiahs-MacBook-Air.local>
2022-06-05 21:37:07 -0400

    added route counter component

--- klymur/src/app/app.module.ts
+++ klymur/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouteCounterComponent } from './components/route-counter/route-counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RouteCounterComponent
   ],
   imports: [
     BrowserModule,
---
+++ klymur/src/app/components/route-counter/route-counter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'klym-route-counter',
+  templateUrl: './route-counter.component.html',
+  styleUrls: ['./route-counter.component.scss']
+})
+export class RouteCounterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
