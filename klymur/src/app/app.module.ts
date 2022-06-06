2a9640100da3988c5be9887666e557d1be605bd1
Shawn Wilson <swilson@swilson-macbookpro-2019.lan>
2022-06-05 22:37:33 -0400

    added timer component

--- klymur/src/app/app.module.ts
+++ klymur/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouteTimerComponent } from './components/route-timer/route-timer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RouteTimerComponent
   ],
   imports: [
     BrowserModule,
---
+++ klymur/src/app/components/route-timer/route-timer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'klym-route-timer',
+  templateUrl: './route-timer.component.html',
+  styleUrls: ['./route-timer.component.scss']
+})
+export class RouteTimerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
