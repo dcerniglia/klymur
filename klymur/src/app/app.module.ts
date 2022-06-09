85f91f59b9c5a6d1dd9b4f189a7201c107ca60b9
Shawn Wilson <swilson@swilson-macbookpro-2019.lan>
2022-06-08 21:33:44 -0400

    added route-selector component

--- klymur/src/app/app.module.ts
+++ klymur/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouteSelectorComponent } from './route-selector/route-selector.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RouteSelectorComponent
   ],
   imports: [
     BrowserModule,
---
+++ klymur/src/app/route-selector/route-selector.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'klym-route-selector',
+  templateUrl: './route-selector.component.html',
+  styleUrls: ['./route-selector.component.scss']
+})
+export class RouteSelectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
